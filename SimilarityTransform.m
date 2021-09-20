1edfdc556740cd044386abaf879cc16398e0c61e
gsp1405 <spalj61@gmail.com>
2021-09-20 23:03:38 +0200

    adjust the similarity transform function

--- SimilarityTransform.m
+++ SimilarityTransform.m
@@ -3,15 +3,18 @@ function [s, R, t] = SimilarityTransform(tem, ref)
 %and performs a similarity transformation (scaling, rotation and
 %translation) in order to optimally transform the landmarks of the template
 %image to the reference image
+    %Center the landmark sets
+    temCenter = tem - mean(tem, 1);
+    refCenter = ref - mean(ref, 1);
     %Define the matrix H
     H = zeros([2, 2]);
     for i = 1:size(tem, 1)
-        H = H + tem(i, :)' * ref(i, :);
+        H = H + temCenter(i, :)' * refCenter(i, :);
     end
-    %Perform singular value decomposition of matrix H
+    %Perform singular value decomposition of matrix
     [U, ~, V] = svd(H);
     R = V * diag([1 det(V * U)]) * U';
-    s = sum(diag(tem * R * ref')) / sum(tem.^2, "all");
+    s = sum(diag(temCenter * R * refCenter')) / sum(temCenter.^2, "all");
     xM = mean(tem, 1);
     yM = mean(ref, 1);
     t = yM' - s * R * xM';
